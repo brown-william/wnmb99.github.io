fa7371c7d03ef2c430d19858d95a5a5b85360529
wnmb99 <wnmb99@gmail.com>
2019-11-01 00:09:10 -0600

    changes again

--- lesson7/js/lazyjava.js
+++ lesson7/js/lazyjava.js
@@ -6,6 +6,13 @@ const imgOptions = {
     rootMargin: "0px 0px 50px 0px"
 };
 
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+    image.removeAttribute('data-src');
+    };
+};
+
 if ('IntersectionObserver' in window) {
 const observer = new IntersectionObserver((items, observer) => {
     items.forEach((item) => {
