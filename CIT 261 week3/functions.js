a8cd9472ae1cf4d24f058ca3a5b978bf7948ed6b
wnmb99 <49930283+wnmb99@users.noreply.github.com>
2020-03-14 17:11:19 -0600

    changes

--- CIT 261 week3/functions.js
+++ CIT 261 week3/functions.js
@@ -1,16 +1,49 @@
-var first = ["a Master", "an Apprentice", "a Novice", "an Expert", "a Grandmaster"];
+let first = ["a Master", "an Apprentice", "a Novice", "an Expert", "a Grandmaster"];
 
 //variable that randomizes the array value of "first"
-var rand = first[Math.floor(Math.random() * first.length)];
+let rand = first[Math.floor(Math.random() * first.length)];
 
 //list of students names
-var students = ["John ", "Jimmy ", "William ", "Isaiah ", "Thomas "];
+let students = ["John ", "Jimmy ", "William ", "Isaiah ", "Thomas "];
 
 //variable that randomizes the array value of "students"
-var randS = students[Math.floor(Math.random() * students.length)];
+let randS = students[Math.floor(Math.random() * students.length)];
+
+
+    
+    for (i = 0; i < first.length; i++) {
+        
+        document.getElementById('example2').innerHTML += first[i] + " " + students[i] + "<br>";
+        
+    }
+ 
+  
+
+function doSomething(name, age) {
+    let result = name + " " + age;
+    return result;
+
+}
+
+document.getElementById("example3").innerHTML = doSomething("John", 32) + "<br>" + doSomething("william", 35) + "<br>" + doSomething("Allicia", 50);
 
 
 
 
 
-document.getElementById("function").innerHTML = "today " + randS + "is " + rand;
+document.getElementById("example1").innerHTML = "today " + randS + "is " + rand;
+
+const theButton1 = document.querySelector(".fillIn");
+
+theButton1.addEventListener("click", function(){
+    
+    const theColor = document.querySelector(".color");
+    const theAnimal = document.querySelector(".animal");
+    const theObject = document.querySelector(".object");
+    const storyValues = {color: 'white', animal: 'rabbit', object: 'log'};
+
+    theColor.textContent = storyValues.color;
+    theAnimal.textContent = storyValues.animal;
+    theObject.textContent = storyValues.object;
+    
+});
---
+++ CIT261week7/events.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var first = document.getElementById('first');
+    first.style.marginLeft = "500px";
+
+    var second = document.getElementById('second');
+    second.style.marginBottom = "700px";
+};
+
